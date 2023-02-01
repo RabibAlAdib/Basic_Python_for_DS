a9036038cccba3d05fce4e56929ee6aa2ce76ed1
Rabib Al Adib <rabib303@gmail.com>
2023-02-02 02:40:18 +0600

    Add files via upload

---
+++ 1. variable.py
@@ -0,0 +1,52 @@
+''' Variable '''
+
+'''
+Variable: Variable is something that contain the location or address for a specific data.
+Which is also known as the storage of a Data.
+'''
+'''
+We can't use anytype of name in a variable.
+
+Valid Variable Way   :   abc10c , ABC_DE , _ , _AbC
+Invalid Variable Way :   99: , 9ABC , X + Y , For
+
+Example:
+'''
+
+# a = 'harry'
+# a = "harry"
+# b = 345
+# c = 45.32
+# d = True
+# d = None
+
+
+# A10 = 5
+# _C_E = 6
+# print(int(A10) + int(_C_E))
+
+'''-----------------------------------------'''
+
+# Use of variable:
+
+# town = "Jessore"
+# name = "ABCD"
+
+# print("My name is {} and I'm from {}.".format(name,town))
+#
+# a = "My name is %a and I'm from...." %name
+# print(a)
+#
+# b = "My name is {} and I'm from {}."
+# b2 = b.format(name, town)
+# print(b2)
+#
+# c = f"My name is {name} and I'm from.{town}"
+# print(c)
+
+'''
+NOTE: * when we use those variables in some function then they'll be called as a parameter.
+'''
+
+#
+
---
+++ 4. Datatype.py
@@ -0,0 +1,32 @@
+''' Data Type '''
+"""******************* Data Types *********************
+
+Variable can hold values and values have data types.
+Data Types:
+    Numeric:   Integer(int)        Complex       Float(float)
+                   2,3,4            x ,y, z      2.4 , 3.5 , 4.8
+
+    Dictionary(dict):  dict = { 'Key':'Value' , 'Ab' : 3 , 'Tom': '50'}
+
+    Boolean:  True   |   False
+
+    Sets(set):  set = {1,2,5}
+
+    Sequence type:       Strings               List               Tuple
+                     '1 , 3 , A, B'       [1 , 3 , A, B]     (1 , 3 , A, B)
+
+"""
+
+a = 5
+b = int(10)
+
+# print(type(a))
+# print(type(b)
+
+# sum = a + b
+# print(sum)
+
+# divide1  = b / a
+# divide2  = float(b) / float(a)
+result = {'divide_1': "Dv1", "divide2": "DV2"}
+print(result['divide_1'])
